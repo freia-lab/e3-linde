61f744fdc91c5948ef38cfb9ab37036a49822cea
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2015-05-22 14:51:22 +0200

    Added command file for setting the deadbands (archive and monitor)

--- src/main/epics/iocBoot/iochnoss/userPostDriverConf.cmd
+++ src/main/epics/iocBoot/iochnoss/userPostDriverConf.cmd
@@ -2,3 +2,5 @@
 ## User provided PLC or fast controller driver post configuration ##
 ####################################################################
 dbpf CstatH-2Kpumps:Ctrl:cCp2Local-evnt.PROC 0
+
+< "${TOP}/iocBoot/iochnoss/setDeadbands.cmd"
