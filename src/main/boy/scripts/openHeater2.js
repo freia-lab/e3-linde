d0b79abb0c4d301be0fe682861acfd076415e0a1
nicklas_prytz <freiaop@freia-consVM.localdomain>
2015-07-29 13:59:47 +0200

    Operator screens defined for: Global Operation View, Purging System, Helium Pumping and Reheating, Supercritical Helium, Interconnection Box - LHe, Interconnection Box - LN2, Liquid Nitrogen. These screens are all defined in separate opi-files in the folder helper-opi-files. For functionality reasons the buttons that redirect the user from screen to screen, that is changes tab, are defined in Hnoss-top.opi.

--- src/main/boy/scripts/openHeater2.js
+++ src/main/boy/scripts/openHeater2.js
@@ -5,4 +5,4 @@ mac=DataUtil.createMacrosInput(false);
 
 mac.put("Heater2", widget.getPropertyValue("name"));
 
-ScriptUtil.openOPI(widget, "helper-opi-files/CstatH-Heater2.opi", 7, mac);
+ScriptUtil.openOPI(widget, "../helper-opi-files/CstatH-Heater2.opi", 7, mac);
