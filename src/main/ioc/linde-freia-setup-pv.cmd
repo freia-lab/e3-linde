c2a5036a0be5324b5dd2c3cb57feba9a2318d55a
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2017-11-24 15:29:29 +0100

    Defined alarm levels and HYST field for some parameters.

--- src/main/ioc/linde-freia-setup-pv.cmd
+++ src/main/ioc/linde-freia-setup-pv.cmd
@@ -1,5 +1,15 @@
 dbpf Cryo-Dwr:LHe:LI5200_Lt.LOLO 200.
 dbpf Cryo-Dwr:LHe:LI5200_Lt.LLSV "MAJOR"
+dbpf Cryo-Dwr:LHe:LI5200_Lt.HYST 5
 
 dbpf Cryo-Cmp:HP:PI2170.LLSV "MAJOR"
 dbpf Cryo-Cmp:HP:PI2170.LOLO 5.5
+dbpf Cryo-Cmp:HP:PI2170.HYST 1.0
+
+# LN2 nytt
+dbpf Cryo-CBox:LN2:LI3685-PercOut.LOW 50
+dbpf Cryo-CBox:LN2:LI3685-PercOut.LOLO 20
+dbpf Cryo-CBox:LN2:LI3685-PercOut.LSV "MINOR"
+dbpf Cryo-CBox:LN2:LI3685-PercOut.LLSV "MAJOR"
+dbpf Cryo-CBox:LN2:LI3685-PercOut.HYST 2
+
